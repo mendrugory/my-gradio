c4b93656a2912e5040c3a5e1b0cb838b7e5f58ca
mendrugory <iam@mendrugory.com>
2023-02-09 09:05:52 +0100

    no task

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import gradio
-from clearml import Task
-
-task = Task.init()
 
 
 def greet(name):
     return "Hello " + name + "!"
 
-demo = gradio.Interface(fn=greet, inputs="text", title=f'Hello', outputs="text")
+
+demo = gradio.Interface(fn=greet, inputs="text", title='Hello without clearml Task', outputs="text")
 demo.launch()
