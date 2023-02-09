df4131d423d965d32be68d12972697b3514a4220
mendrugory <iam@mendrugory.com>
2023-02-09 09:34:14 +0100

    patching

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import gradio
+from clearml import Task
 
-import patching
+task = Task.init()
+
 
 def greet(name):
     return "Hello " + name + "!"
+
 
 demo = gradio.Interface(fn=greet, inputs="text", title=f'Hello', outputs="text")
 demo.launch()
