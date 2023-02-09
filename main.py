b150eedede4cf0f6ee17c02b2a0fc57e125a0005
mendrugory <iam@mendrugory.com>
2023-02-09 11:35:57 +0100

    test

--- main.py
+++ main.py
@@ -8,5 +8,7 @@ def greet(name):
     return "Hello " + name + "!"
 
 
-demo = gradio.Interface(fn=greet, inputs="text", title='Hello', outputs="text")
+demo = gradio.Interface(
+    fn=greet, inputs="text", title='Hello from main', outputs="text"
+)
 demo.launch()
